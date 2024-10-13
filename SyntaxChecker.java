400391722e3d3b44e9dfa25907176710bdccbd26
PancakesForRent <astenbitancur@gmail.com>
2024-10-13 14:49:54 +0800

    Finalized tokenizer function

--- SyntaxChecker.java
+++ SyntaxChecker.java
@@ -1,6 +1,8 @@
+import java.util.Arrays;
+
 public class SyntaxChecker {
     public static void main(String[] args){
-        String sample = "for ( int i = 9 ; i < 0 ; i++ ) { System.out.println ( \"hello\" ) ; }";
+        String sample = "for ( int i = 9 ; i > 0 ; i-- ) { System.out.println ( \"hello\" ) ; }";
         String[] sampleToken = tokenizer(sample);
         for(String i: sampleToken){
             System.out.println(i);
@@ -27,10 +29,49 @@ public class SyntaxChecker {
                 case ")":
                     tokenizedStrings[i] = "closeParen";
                     break;
+                case "int":
+                    tokenizedStrings[i] = "intType";
+                    break;
+                case "=":
+                    tokenizedStrings[i] = "assign";
+                    break;
+                case ";":
+                    tokenizedStrings[i] = "semiCol";
+                    break;
+                case "System.out.print":
+                    tokenizedStrings[i] = "printStmt";
+                    break;
+                case "System.out.println":
+                    tokenizedStrings[i] = "printStmt";
+                    break;
+                default:
+                    String comparisonOptr[] = {"==", "!=", ">=", "<=", ">", "<"};
+                    if (Arrays.asList(comparisonOptr).contains(untokenizedStrings[i])) {
+                        tokenizedStrings[i] = "comparisonOptr";
+                        break;
+                    }else if (untokenizedStrings[i].matches("\"(\\w*\\s*\\'*\\,*)*\"")){
+                        tokenizedStrings[i] = "string";
+                        break;
+                    }else if (untokenizedStrings[i].matches("\\d")){
+                        tokenizedStrings[i] = "integer";
+                        break;
+                    }else if (untokenizedStrings[i].matches("(([a-zA-Z]+|_+)+(\\d*|[a-zA-Z]*|_*)*)+")){
+                        tokenizedStrings[i] = "variable";
+                        break;
+                    }else if (untokenizedStrings[i].matches("(([a-zA-Z]+|_+)+(\\d*|[a-zA-Z]*|_*)*)+\\+\\+")) {
+                        tokenizedStrings[i] = "incVar";
+                        break;
+                    }else if (untokenizedStrings[i].matches("(([a-zA-Z]+|_+)+(\\d*|[a-zA-Z]*|_*)*)+\\-\\-")) {
+                        tokenizedStrings[i] = "decVar";
+                        break;
+                    }else{
+                        tokenizedStrings[i] = "unknown";
+                        break;
+                    }
                 
             }
 
         }
-        return untokenizedStrings;
+        return tokenizedStrings;
     }
 }
